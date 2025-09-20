a2ff81a7422eccaea403fb9aebb2cd1148baf991
bahacan112-lenovo <bzenbil19@gmail.com>
2025-09-21 01:31:33 +0300

    Remove unnecessary components from nachhaltig page (NachhaltigMission, NachhaltigInitiatives, NachhaltigImpact, NachhaltigPartners)

--- src/app/nachhaltig/page.tsx
+++ src/app/nachhaltig/page.tsx
@@ -2,10 +2,6 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import NachhaltigHero from "@/components/NachhaltigHero";
 import Breadcrumb from "@/components/Breadcrumb";
-import NachhaltigMission from "@/components/NachhaltigMission";
-import NachhaltigInitiatives from "@/components/NachhaltigInitiatives";
-import NachhaltigImpact from "@/components/NachhaltigImpact";
-import NachhaltigPartners from "@/components/NachhaltigPartners";
 import TrustBadgesSection from "@/components/TrustBadgesSection";
 import NachhaltigFeaturedSavingsSection from "@/components/NachhaltigFeaturedSavingsSection";
 import NachhaltigSummary from "@/components/NachhaltigSummary";
@@ -33,10 +29,6 @@ export default function NachhaltigPage() {
       <NachhaltigWhatIs />
       <NachhaltigProcessSection />
       <NachhaltigFAQ />
-      <NachhaltigMission />
-      <NachhaltigInitiatives />
-      <NachhaltigImpact />
-      <NachhaltigPartners />
       <Footer />
     </div>
   );
